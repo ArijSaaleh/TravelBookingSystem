6617baf7cc482a38b280a4d0a3189e12465e8a93
ArijSaaleh <arij.saleh@esprit.tn>
2024-02-26 22:21:45 +0100

    Initial commit

---
+++ src/main/java/com/arij/travelbookingsystem/TravelBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.arij.travelbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arij/travelbookingsystem/TravelBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arij.travelbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

2ba408e177b093c9bcd8e01ff28ed6b68166b87a
Arij Saleh <79157543+ArijSaaleh@users.noreply.github.com>
2024-02-28 22:55:30 +0100

    Update TravelBookingSystemApplication.java

--- src/main/java/com/arij/travelbookingsystem/TravelBookingSystemApplication.java
+++ src/main/java/com/arij/travelbookingsystem/TravelBookingSystemApplication.java
@@ -8,6 +8,7 @@ public class TravelBookingSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TravelBookingSystemApplication.class, args);
+        //hello
     }
 
 }
